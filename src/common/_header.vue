0a1e706f995278d4ba05a8da8dba2b5d9392776d
1141395281@qq.com <1141395281@qq.com>
2018-05-12 18:10:21 +0800

    ``

--- src/common/_header.vue
+++ src/common/_header.vue
@@ -17,28 +17,27 @@ export default {
 
 <style scoped>
  header{
-   display: flex;
+   position: relative;
    height:0.88rem;
+   line-height: 0.88rem;
    background: url("../assets/image/m-maya-top.jpg") 0 0 repeat-y; background-size: 100% 100%;
    text-align: center;
-   align-items: center;
-   justify-content: space-between;
-   position: relative;
    padding: 0 0.2rem;
    margin-bottom: 0.4rem;
-   box-sizing: content-box;
  }
  header h1{
- font-size:0.36rem;color: #fff;
+  font-size:0.36rem;color: #fff;
    display: block;
+   width: 100%;
    font-weight: 400;
-   margin-right:3.2rem;
-   letter-spacing: 0.2rem;
+   text-align: center;
  }
  header span,header a {
-   display: inline-block;
+   position: absolute;
+   left: 0.3rem;
+   top: 50%;
+   transform:translateY(-50%) rotate(-180deg);
    font-size: 0.46rem;
-   transform: rotate(-180deg);
  }
  .icon-go:before{
    color:#fff;
